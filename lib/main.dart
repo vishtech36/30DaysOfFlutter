97ae03ede21dec9253bda7797e57f500183162c8
Vishwajeet Barve <vishwajeet.barve@cerence.com>
2022-03-18 13:00:10 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'dart:ffi';
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Material(
+            child: Center(
+      child: Text('Welcome to 30 days of flutter'),
+    )));
+  }
+}
